1c1ed3d9763564b9cd69f1487f1e20a7bf55fa11
John David Soto <thedavidsoto@gmail.com>
2022-08-06 17:47:03 +0800

    Refactor code to make the UI functional. Game can now be played on browser

--- script.js
+++ script.js
@@ -1,84 +1,88 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
+function printScore() {
+  document.getElementById("player-score").innerHTML = `Player Score: ${playerScore}`;
+  document.getElementById("computer-score").innerHTML = `Computer Score: ${computerScore}`;
+}
+// Get computer choice
+function getComputerChoice() {
+  // Randomize computer choice from rock, paper, scissors
+  // Get random number among 0, 1, ,2
+  let value = Math.floor(Math.random() * 3);
+  // If random number = 0, then computer choice is rock
+  if (value === 0) {
+    return "ROCK";
+    // If random number = 1, then computer choice is paper
+  } else if (value === 1) {
+    return "PAPER"
+    // If random number = 2, then computer choice is scissors
+  } else {
+    return "SCISSORS";
+  }
+}
+
+function playRound(playerChoice, computerChoice) {
+  //initialize playerScore and computerScore
+  document.getElementById("player-choice").innerHTML = `Player choice is ${playerChoice}`;
+  document.getElementById("computer-choice").innerHTML = `Computer choice is ${computerChoice}`;
+  // Check user versus computer choice
+  if (playerChoice === "ROCK" && computerChoice === "PAPER") {
+    computerScore++;
+    document.getElementById("result").innerHTML = "You lose! Paper beats rock!";
+    printScore();
+  } else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+    playerScore++;
+    document.getElementById("result").innerHTML = "You win! Rock beats scissors!";
+    printScore();
+  } else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+    playerScore++;
+    document.getElementById("result").innerHTML = "You win! Paper beats rock!";
+    printScore();
+  } else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
+    computerScore++;
+    document.getElementById("result").innerHTML = "You lose! Scissors beats paper!";
+    printScore();
+  } else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
+    computerScore++;
+    document.getElementById("result").innerHTML = "You lose! Rock beats scissors!";
+    printScore();
+  } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+    playerScore++;
+    document.getElementById("result").innerHTML = "You win! Paper beats scissors!";
+    printScore();
+  } else {
+    // Same choices means tie, therefore no winner
+    document.getElementById("result").innerHTML = "Draw!";
+  }
+  checkScore();
+}
+
+function checkScore() {
+  if (playerScore === 5 && computerScore < 5) {
+    document.getElementById("winner").innerHTML = "You got 5 points first. You win!";
+    playerScore = 0, computerScore = 0, roundNumber = 0;
+    document.getElementById("round-number").innerHTML = `Round #${roundNumber}`;
+    printScore();
+  } else if (computerScore === 5 && playerScore < 5) {
+    document.getElementById("winner").innerHTML = "The computer got 5 points first. You lose!";
+    playerScore = 0, computerScore = 0, roundNumber = 0;
+    document.getElementById("round-number").innerHTML = `Round #${roundNumber}`;
+    printScore();
+  }
+}
+
 // Prompt player for choice
 function getPlayerChoice() {
-    let playerChoice = prompt("Please enter choice: ");
-    // convert player prompt as non case-sensitive
-    return playerChoice.toUpperCase();
-  }
-
-  // Get computer choice
-  function getComputerChoice() {
-    // Randomize computer choice from rock, paper, scissors
-    // Get random number among 0, 1, ,2
-    let value = Math.floor(Math.random() * 3);
-    // If random number = 0, then computer choice is rock
-    if (value === 0) {
-      return "ROCK";
-      // If random number = 1, then computer choice is paper
-    } else if (value === 1) {
-      return "PAPER"
-      // If random number = 2, then computer choice is scissors
-    } else {
-      return "SCISSORS";
+  //declare querySelector for three choices
+  const btn = document.querySelectorAll('button');
+  btn.forEach((buttons) => buttons.addEventListener('click', () => {
+    playRound(buttons.value, getComputerChoice());
+    roundNumber++;
+    document.getElementById("round-number").innerHTML = `Round #${roundNumber}`;
     }
-  }
+  ));
+}
 
-  function playRound() {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-
-    console.log(`Player choice is ${playerChoice}`);
-    console.log(`Computer choice is ${computerChoice}`);
-
-    // Check user versus computer choice
-    // Rock beats scissors
-    // Scissors beats paper
-    // Paper beats rock
-    if (playerChoice === "ROCK" && computerChoice === "PAPER") {
-      console.log("You lose! Paper beats rock!");
-      return 1;
-    } else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
-      console.log("You win! Rock beats scissors!");
-      return 0;
-    } else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
-      console.log("You win! Paper beats rock!");
-      return 0;
-    } else if (playerChoice === "PAPER" && computerChoice === "SCISSORS") {
-      console.log("You lose! Scissors beats paper!");
-      return 1;
-    } else if (playerChoice === "SCISSORS" && computerChoice === "ROCK") {
-      console.log("You lose! Rock beats scissors!");
-      return 1;
-    } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
-      console.log("You win! Paper beats scissors!");
-      return 0;
-    } else {
-      // Same choices means tie, therefore no winner
-      console.log("Draw!");
-    }
-  }
-
-
-  function game() {
-    // Tally score for winner (if user or computer)
-    // Initialize score for user = 0 and computer = 0
-    let userScore = 0;
-    let computerScore = 0;
-
-    // Play 5 rounds of the game
-    for (let i = 0; i < 5; i++) {
-      whoWins = playRound();
-      if (whoWins === 0) {
-        // If user wins, userScore =+ 1
-        userScore++;
-        console.log(userScore);
-        console.log(computerScore);
-      } else if (whoWins === 1) {
-        // If computer wins, computerScore += 1
-        computerScore++;
-        console.log(userScore);
-        console.log(computerScore);
-      }
-    }
-  }
-
-  game();
+getPlayerChoice();
